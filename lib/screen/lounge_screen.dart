b255e1954745f10eeebb4e22b9c9048865ef6a2a
Hiroaki-hey-jude <hiroakiueda2000@gmail.com>
2022-12-18 14:57:57 +0800

    background timer is done

--- lib/screen/lounge_screen.dart
+++ lib/screen/lounge_screen.dart
@@ -6,6 +6,8 @@ import 'package:toggle_switch/toggle_switch.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../shared/constant.dart';
+
 class LoungeScreen extends ConsumerWidget {
   const LoungeScreen({super.key});
 
@@ -41,6 +43,7 @@ class LoungeScreen extends ConsumerWidget {
           SafeArea(
             child: Column(
               children: [
+                const SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -58,29 +61,115 @@ class LoungeScreen extends ConsumerWidget {
                     ),
                   ],
                 ),
+                const SizedBox(height: 50),
                 Text(
                   DateFormat.ms().format(state.time),
-                  style: Theme.of(context).textTheme.headline2,
+                  style: Theme.of(context).textTheme.headline1,
                 ),
+                const SizedBox(height: 50),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    FloatingActionButton(
-                      onPressed: () {
-                        if (state.timer != null && state.timer!.isActive) {
-                          state.timer!.cancel();
-                        }
-                      },
-                      child: const Text("Stop"),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3, //横幅
+                      height: 50, //高さ
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          side: const BorderSide(
+                            color: Colors.white,
+                          ),
+                          backgroundColor: Colors.transparent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        onPressed: () {
+                          notifier.startTimer();
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(Icons.play_arrow),
+                            SizedBox(width: 8),
+                            Text('Start'),
+                          ],
+                        ),
+                      ),
                     ),
-                    FloatingActionButton(
-                      onPressed: () {
-                        notifier.startTimer();
-                      },
-                      child: const Text("Start"),
+                    SizedBox(width: 10),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3, //横幅
+                      height: 50, //高さ
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          side: const BorderSide(
+                            color: Colors.white,
+                          ),
+                          backgroundColor: Colors.transparent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (state.timer != null && state.timer!.isActive) {
+                            state.timer!.cancel();
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(Icons.clear),
+                            SizedBox(width: 8),
+                            Text('Reset'),
+                          ],
+                        ),
+                      ),
                     ),
                   ],
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 40,
+                  ),
+                  child: Container(
+                    width: double.infinity,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Constant.whiteColor,
+                      ),
+                      color: Constant.whiteColor.withOpacity(0.3),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
+                            offset: Offset(0, 10))
+                      ],
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 8),
+                        Text(
+                          'Lounge',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Constant.blackColor,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
--- lib/state/lounge_state/lounge_state.dart
+++ lib/state/lounge_state/lounge_state.dart
@@ -33,10 +33,9 @@ class LoungeStateNotifier extends StateNotifier<LoungeState>
     with WidgetsBindingObserver {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
-  PomodoloModel workTime = PomodoloModel(status: Status.initial, time: 25);
-  PomodoloModel restTime = PomodoloModel(status: Status.initial, time: 5);
+  PomodoloModel pomodoloModel = PomodoloModel(status: Status.initial, time: 25);
   // PomodoloModel workTime = PomodoloModel(25, Status.initial);
-  bool _isTimerPaused = false; // バックグラウンドに遷移した際にタイマーがもともと起動中で、停止したかどうか
+  bool? _isTimerPaused; // バックグラウンドに遷移した際にタイマーがもともと起動中で、停止したかどうか
   late DateTime? _pausedTime; // バックグラウンドに遷移した時間
   late int? _notificationId; // 通知ID
   IntervalType intervalType = IntervalType.work;
@@ -63,9 +62,11 @@ class LoungeStateNotifier extends StateNotifier<LoungeState>
         break;
       case AppLifecycleState.paused:
         print('停止されたときの処理');
+        handleOnPaused();
         break;
       case AppLifecycleState.resumed:
         print('再開されたときの処理');
+        handleOnResumed();
         break;
       case AppLifecycleState.detached:
         print('破棄されたときの処理');
@@ -95,22 +96,31 @@ class LoungeStateNotifier extends StateNotifier<LoungeState>
 
   // タイマーを開始する
   void startTimer() {
-    state = state.copyWith(
-        timer: Timer.periodic(const Duration(seconds: 1), (Timer timer) {
-      state = state.copyWith(time: state.time.add(const Duration(seconds: -1)));
-      handleTimeIsOver();
-    }));
+    if (pomodoloModel.status == Status.initial || pomodoloModel.status == Status.stopped) {
+      print(pomodoloModel.status);
+      print(intervalType);
+      state = state.copyWith(
+          timer: Timer.periodic(const Duration(seconds: 1), (Timer timer) {
+        state =
+            state.copyWith(time: state.time.add(const Duration(seconds: -1)));
+        handleTimeIsOver();
+      }));
+      pomodoloModel = pomodoloModel.copyWith(status: Status.started);
+    }
   }
 
   void workOrRest(bool value) {
     if (value == true) {
+      intervalType = IntervalType.rest;
       state = state.copyWith(isResting: true);
       state = state.copyWith(
         time: DateTime.utc(0, 0, 0).add(
           const Duration(minutes: 5),
         ),
       );
+      print(intervalType);
     } else {
+      intervalType = IntervalType.work;
       state = state.copyWith(isResting: false);
       state = state.copyWith(
         time: DateTime.utc(0, 0, 0).add(
@@ -118,5 +128,71 @@ class LoungeStateNotifier extends StateNotifier<LoungeState>
         ),
       );
     }
+  }
+
+  //アプリがバックグラウンドに行った時の処理
+  void handleOnPaused() {
+    if (state.timer == null) {
+      return;
+    }
+    if (state.timer!.isActive) {
+      _isTimerPaused = true;
+      state.timer!.cancel(); // タイマーを停止する
+      pomodoloModel = pomodoloModel.copyWith(status: Status.stopped);
+      print('timer cancelled');
+    }
+    _pausedTime = DateTime.now(); // バックグラウンドに遷移した時間を記録
+    _notificationId = scheduleLocalNotification(
+        state.time.difference(DateTime.utc(0, 0, 0))); // ローカル通知をスケジュール登録
+  }
+
+  void handleOnResumed() {
+    if (_isTimerPaused == null) {
+      return;
+    } // タイマーが動いてなければ何もしない
+    Duration backgroundDuration =
+        DateTime.now().difference(_pausedTime!); // バックグラウンドでの経過時間
+
+    // バックグラウンドでの経過時間が終了予定を超えていた場合（この場合は通知実行済みのはず）
+    if (state.time
+            .difference(DateTime.utc(0, 0, 0))
+            .compareTo(backgroundDuration) <
+        0) {
+      state = state.copyWith(time: DateTime.utc(0, 0, 0)); // 時間をリセットする
+    } else {
+      state = state.copyWith(
+          time: state.time.add(-backgroundDuration)); // バックグラウンド経過時間分時間を進める
+      startTimer(); // タイマーを再開する
+      print('timer resterted');
+    }
+    if (_notificationId != null) {
+      flutterLocalNotificationsPlugin.cancel(_notificationId!); // 通知をキャンセル
+    }
+    _isTimerPaused = false; // リセット
+    _notificationId = null; // リセット
+    _pausedTime = null;
+  }
+
+  int scheduleLocalNotification(Duration duration) {
+    print('notification set');
+    flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(
+        iOS: DarwinInitializationSettings(),
+      ), // app_icon.pngを配置
+    );
+    int notificationId = DateTime.now().hashCode;
+    flutterLocalNotificationsPlugin.zonedSchedule(
+      notificationId,
+      'Time is over',
+      '頑張ってるね',
+      tz.TZDateTime.now(tz.local).add(duration),
+      const NotificationDetails(
+        iOS: DarwinNotificationDetails(),
+      ),
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+    );
+    return notificationId;
   }
 }
