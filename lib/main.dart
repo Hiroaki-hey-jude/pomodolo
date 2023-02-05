3f0cd517aa26745fdc561453911950af6d9f2a34
Hiroaki-hey-jude <hiroakiueda2000@gmail.com>
2023-02-06 00:58:16 +0800

    強制アップデート追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pomodolo/data/sharedpreference/sharedPreferencesData.dart';
 import 'package:pomodolo/screen/auth/login_screen.dart';
 import 'package:pomodolo/screen/auth/register_screen.dart';
 import 'package:pomodolo/screen/home_screen.dart';
 import 'package:pomodolo/screen/lounge_screen.dart';
 import 'package:pomodolo/shared/constant.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:version/version.dart';
 
 void main() async {
   //_setupTimeZone();
@@ -61,6 +65,27 @@ class _MyAppHomeState extends State<MyAppHome> {
     print("UUID: $uuid");
   }
 
+  Future<void> versionCheck() async {
+    //アプリのバージョンを取得
+    final info = await PackageInfo.fromPlatform();
+    final currentVersion = Version.parse(info.version);
+    print('今のバージョン$currentVersion');
+
+    //Firestoreからアップデートしたいバージョンを取得
+    final doc = await FirebaseFirestore.instance
+        .collection('config')
+        .doc('GFtr9GyeYHg1BEszyCUT')
+        .get();
+    final newVersion =
+        Version.parse(doc.data()!['ios_force_app_version'].toString());
+
+    //バージョンを比較し、現在のバージョンの方が低ければダイアログを出す
+    if (currentVersion < newVersion) {
+      print('hhhehehhe');
+      showUpdateDialog(context);
+    }
+  }
+
   bool _isSignedIn = false;
   @override
   void initState() {
@@ -68,6 +93,7 @@ class _MyAppHomeState extends State<MyAppHome> {
     super.initState();
     getUserLoggedInStatus();
     initPlugin();
+    versionCheck();
   }
 
   getUserLoggedInStatus() async {
@@ -106,4 +132,37 @@ class _MyAppHomeState extends State<MyAppHome> {
           ],
         ),
       );
+}
+
+void showUpdateDialog(BuildContext context) {
+  showDialog<void>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      const title = 'バージョン更新のお知らせ';
+      const message = '新しいバージョンのアプリが利用可能です。ストアより更新版を入手して、ご利用下さい。';
+      const btnLabel = '今すぐ更新';
+      return AlertDialog(
+        title: const Text(title),
+        content: const Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: const Text(
+              btnLabel,
+              style: TextStyle(color: Colors.red),
+            ),
+            onPressed: () async {
+              if (await canLaunchUrl(Constant().appStoreURL)) {
+                await launchUrl(
+                  Constant().appStoreURL,
+                );
+              } else {
+                throw Error();
+              }
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
